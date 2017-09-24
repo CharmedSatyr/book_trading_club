573c16b877276be1cb7ac30753e1af708ca8e1ac
CharmedSatyr <z@charmed.tech>
2017-09-25 01:09:35 +0900

    Improved security of User schema

--- server/models/User.js
+++ server/models/User.js
@@ -4,8 +4,18 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const User = new Schema({
-  username: String,
-  password: String,
+  username: { type: String, index: { unique: true } },
+  password: {
+    type: String,
+    required: true,
+    match: /(?=.*[a-zA-Z])(?=.*[0-9]+).*/,
+    minlength: 12
+  },
+  created: {
+    type: Date,
+    required: true,
+    default: new Date()
+  },
   location: String,
   shelves: []
 })
