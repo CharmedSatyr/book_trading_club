903f5ef76b432fd6f98d6ad679354408eb67907c
CharmedSatyr <z@charmed.tech>
2017-10-01 14:55:09 +0900

    NavBar buttons collapse if window width < 900

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -13,12 +13,6 @@ import IconMenu from 'material-ui/IconMenu'
 import MenuItem from 'material-ui/MenuItem'
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
 
-/*** VARIABLES ***/
-const style = {
-  marginTop: 7,
-  textDecoration: 'none'
-}
-
 /*** MAIN ***/
 const NavBar = ({
   addbooksfn,
@@ -34,40 +28,62 @@ const NavBar = ({
         iconElementLeft={<AvLibraryBooks style={{ marginTop: 12 }} />}
         iconElementRight={
           <span>
-            <div className="welcomeName">Welcome, {loggedUser}</div>
-            <IconMenu
-              iconButtonElement={
-                <IconButton>
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-              anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-            >
-              <MenuItem
-                primaryText="My Books"
-                onClick={mybooksfn}
-                style={style}
-              />
-              <MenuItem
-                primaryText="Other Books"
-                onClick={allbooksfn}
-                style={style}
-              />
-              <MenuItem
-                primaryText="Add Books"
-                onClick={addbooksfn}
-                style={style}
-              />
-              <MenuItem
-                primaryText="Profile"
-                onClick={profilefn}
-                style={style}
-              />
-              <a href="/logout">
-                <MenuItem primaryText="Logout" style={style} />
-              </a>
-            </IconMenu>
+            {window.innerWidth < 900 ? (
+              <span>
+                <div className="welcomeName">Welcome, {loggedUser}</div>
+                <IconMenu
+                  iconButtonElement={
+                    <IconButton>
+                      <MoreVertIcon />
+                    </IconButton>
+                  }
+                  targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+                  anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+                >
+                  <MenuItem primaryText="Add Books" onClick={addbooksfn} />
+                  <MenuItem primaryText="My Books" onClick={mybooksfn} />
+                  <MenuItem primaryText="Other Books" onClick={allbooksfn} />
+                  <MenuItem primaryText="Profile" onClick={profilefn} />
+                  <a href="/logout">
+                    <MenuItem
+                      primaryText="Logout"
+                      style={{
+                        textDecoration: 'none'
+                      }}
+                    />
+                  </a>
+                </IconMenu>
+              </span>
+            ) : (
+              <span>
+                <div className="welcomeName expanded">
+                  Welcome, {loggedUser}
+                </div>
+                <FlatButton
+                  label="Add Books"
+                  onClick={addbooksfn}
+                  className="navButtons"
+                />
+                <FlatButton
+                  label="My Books"
+                  onClick={mybooksfn}
+                  className="navButtons"
+                />
+                <FlatButton
+                  label="Other Books"
+                  onClick={allbooksfn}
+                  className="navButtons"
+                />
+                <FlatButton
+                  label="Profile"
+                  onClick={profilefn}
+                  className="navButtons"
+                />
+                <a href="/logout">
+                  <FlatButton label="Logout" className="navButtons" />
+                </a>
+              </span>
+            )}
           </span>
         }
       />
