da29ebac956e231e068038390456e75ef4e5c9ea
CharmedSatyr <z@charmed.tech>
2017-12-13 10:19:27 +0900

    Restructured WhichButton into Component with badly working Snacks.

---
+++ client/components/Snack.jsx
@@ -0,0 +1,53 @@
+'use strict'
+
+/*** COMPONENTS ***/
+//React
+import React, { Component } from 'react'
+
+//Material UI
+import Snackbar from 'material-ui/Snackbar'
+
+/*** MAIN ***/
+export default class Snack extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false,
+      message: ''
+    }
+    this.handleRequestClose = this.handleRequestClose.bind(this)
+    this.timer = undefined
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.message.length > 0) {
+      this.setState({
+        message: nextProps.message,
+        open: true
+      })
+    }
+  }
+  handleRequestClose() {
+    this.setState({
+      open: false
+    })
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state === nextState) {
+      return false
+    } else {
+      return true
+    }
+  }
+  render() {
+    const { message, open } = this.state
+    return (
+      <Snackbar
+        action=""
+        autoHideDuration={3000}
+        message={message}
+        onRequestClose={this.handleRequestClose}
+        open={open}
+      />
+    )
+  }
+}
--- client/components/WhichButton.jsx
+++ client/components/WhichButton.jsx
@@ -2,13 +2,16 @@
 
 /*** COMPONENTS ***/
 //React
-import React from 'react'
+import React, { Component } from 'react'
 
 //Material UI
-import IconButton from 'material-ui/IconButton'
-import NavigationClose from 'material-ui/svg-icons/navigation/close'
-import NavigationCheck from 'material-ui/svg-icons/navigation/check'
 import ActionSwapVerticalCircle from 'material-ui/svg-icons/action/swap-vertical-circle'
+import IconButton from 'material-ui/IconButton'
+import NavigationCheck from 'material-ui/svg-icons/navigation/check'
+import NavigationClose from 'material-ui/svg-icons/navigation/close'
+
+//App
+import Snack from './Snack.jsx'
 
 //Styles
 import { red500, yellow500, blue500, greenA700 } from 'material-ui/styles/colors'
@@ -21,9 +24,17 @@ const approveDenyStyle = {
 /*** FUNCTIONS ***/
 import { f } from '../../common/common.functions.js'
 
-const WhichButton = ({ loggedUser, olkey, owner, requestor, whichButton }) => {
-  if (whichButton === 'delete') {
-    return (
+/*** MAIN ***/
+export default class WhichButton extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { message: '' }
+  }
+  render() {
+    const { loggedUser, olkey, owner, requestor, whichButton } = this.props
+
+    //delete button
+    const del = (
       <div className="bookOverlay">
         <IconButton
           onClick={() => {
@@ -33,14 +44,17 @@ const WhichButton = ({ loggedUser, olkey, owner, requestor, whichButton }) => {
             }
             const data = encodeURIComponent(JSON.stringify(bookInfo))
             f('DELETE', '/api/' + owner + '/save/' + data)
+            this.setState({ message: 'Deleting book!' })
           }}
         >
           <NavigationClose color={red500} />
         </IconButton>
+        <Snack message={this.state.message} />
       </div>
     )
-  } else if (whichButton === 'swap') {
-    return (
+
+    //swap button
+    const swap = (
       <div className="bookOverlay">
         <IconButton
           onClick={() => {
@@ -51,14 +65,17 @@ const WhichButton = ({ loggedUser, olkey, owner, requestor, whichButton }) => {
             const data = encodeURIComponent(JSON.stringify(bookInfo))
             f('POST', '/api/' + requestor + '/request/' + data)
             console.log('We should swap:', bookInfo)
+            this.setState({ message: 'Lets swap!' })
           }}
         >
           <ActionSwapVerticalCircle />
         </IconButton>
+        <Snack message={this.state.message} />
       </div>
     )
-  } else if (whichButton === 'approveDeny') {
-    return (
+
+    //approveDeny button
+    const approveDeny = (
       <div className="approveDeny">
         {/* APPROVE REQUEST */}
         <IconButton
@@ -70,8 +87,9 @@ const WhichButton = ({ loggedUser, olkey, owner, requestor, whichButton }) => {
             const data = encodeURIComponent(JSON.stringify(bookInfo))
             f('POST', '/api/' + loggedUser + '/approveRequest/' + data, request => {
               console.log('Approve Request', request)
+              this.setState({ message: 'Accepted request!' })
               alert(
-                'You have approved a request! In a completed app, you would now be able to send a message via a simple app integration (e.g., https://agile-oasis-2124.herokuapp.com/) to the other user to coordinate the swap.'
+                'You have approved a request! See you at Beached Bar this Tuesday night at 8:30pm.'
               )
             })
           }}
@@ -89,16 +107,19 @@ const WhichButton = ({ loggedUser, olkey, owner, requestor, whichButton }) => {
             const data = encodeURIComponent(JSON.stringify(bookInfo))
             f('POST', '/api/' + loggedUser + '/denyRequest/' + data, request => {
               console.log('Deny Request', request)
+              this.setState({ message: 'Request denied.' })
             })
           }}
           style={approveDenyStyle}
         >
           <NavigationClose color={red500} />
         </IconButton>
+        <Snack message={this.state.message} />
       </div>
     )
-  } else if (whichButton === 'cancelRequest') {
-    return (
+
+    //cancelRequest button
+    const cancelRequest = (
       <div className="bookOverlay">
         <IconButton
           onClick={() => {
@@ -109,16 +130,32 @@ const WhichButton = ({ loggedUser, olkey, owner, requestor, whichButton }) => {
             const data = encodeURIComponent(JSON.stringify(bookInfo))
             f('POST', '/api/' + loggedUser + '/cancelRequest/' + data, response => {
               console.log(response)
+              this.setState({ message: 'Request canceled!' })
             })
           }}
         >
           <NavigationClose color={red500} />
         </IconButton>
+        <Snack message={this.state.message} />
       </div>
     )
-  } else {
-    return null
+
+    //return button based on whichButton prop
+    switch (whichButton) {
+      case 'delete':
+        return del
+        break
+      case 'swap':
+        return swap
+        break
+      case 'approveDeny':
+        return approveDeny
+        break
+      case 'cancelRequest':
+        return cancelRequest
+        break
+      default:
+        return null
+    }
   }
 }
-
-export default WhichButton
