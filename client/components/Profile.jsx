efa894b182db83afb4bb08dfd53b867d584e8f01
CharmedSatyr <z@charmed.tech>
2017-12-16 01:51:45 +0900

    improved profile styling

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -16,81 +16,90 @@ import { f } from '../../common/common.functions.js'
 /*** MAIN ***/
 const Profile = ({ loggedUser }) => {
   return (
-    <div>
-      <Subheader>Update your profile</Subheader>
+    <span>
+      <h2>Update your profile</h2>
       <Divider />
-      <form>
-        <Subheader>Personal Details</Subheader>
-        <TextField
-          hintText="Your username will be public."
-          floatingLabelText="Username"
-          id="username"
-        />
-        <br />
-        <TextField
-          hintText="City and state or province"
-          floatingLabelText="Location"
-          id="location"
-        />
-        <br />
-        <RaisedButton
-          label="Save Changes"
-          primary={true}
-          className="RaisedButtonProfile"
-          onClick={() => {
-            const username = document.getElementById('username').value
-            const location = document.getElementById('location').value
-            const update = { username: username, location: location }
-            const data = encodeURIComponent(JSON.stringify(update))
+      <div className="profileBox">
+        <form>
+          <Subheader>Personal details</Subheader>
+          <TextField
+            fullWidth="true"
+            hintText="Your username will be public."
+            floatingLabelText="Username"
+            id="username"
+          />
+          <br />
+          <TextField
+            fullWidth="true"
+            hintText="City and state or province"
+            floatingLabelText="Location"
+            id="location"
+          />
+          <br />
+          <RaisedButton
+            buttonStyle={{ width: '100%' }}
+            className="RaisedButtonProfile"
+            label="Save Changes"
+            onClick={() => {
+              const username = document.getElementById('username').value
+              const location = document.getElementById('location').value
+              const update = { username: username, location: location }
+              const data = encodeURIComponent(JSON.stringify(update))
 
-            f('POST', '/api/' + loggedUser + '/profile-update/' + data, response => {
-              console.log(response)
-            })
-          }}
-        />
-      </form>
+              f('POST', '/api/' + loggedUser + '/profile-update/' + data, response => {
+                console.log(response)
+              })
+            }}
+            primary={true}
+          />
+        </form>
+      </div>
+
       <br />
       <br />
       <Divider />
-      <form>
-        <Subheader>Change your password</Subheader>
-        <TextField
-          hintText="Use 12-72 letters and numbers."
-          floatingLabelText="Current Password"
-          type="password"
-          id="password1"
-        />
-        <br />
-        <TextField
-          hintText="Use 12-72 letters and numbers."
-          floatingLabelText="New Password"
-          type="password"
-          id="password2"
-        />
-        <br />
-        <RaisedButton
-          label="Save Changes"
-          primary={true}
-          className="RaisedButtonProfile"
-          onClick={() => {
-            const password1 = document.getElementById('password1').value
-            const password2 = document.getElementById('password2').value
+      <div className="profileBox">
+        <form>
+          <Subheader>Change your password</Subheader>
+          <TextField
+            fullWidth="true"
+            hintText="Use 12-72 letters and numbers."
+            floatingLabelText="Current Password"
+            type="password"
+            id="password1"
+          />
+          <br />
+          <TextField
+            fullWidth="true"
+            hintText="Use 12-72 letters and numbers."
+            floatingLabelText="New Password"
+            type="password"
+            id="password2"
+          />
+          <br />
+          <RaisedButton
+            label="Save Changes"
+            primary={true}
+            className="RaisedButtonProfile"
+            onClick={() => {
+              const password1 = document.getElementById('password1').value
+              const password2 = document.getElementById('password2').value
 
-            const bothPasswords = {
-              currentPassword: password1,
-              newPassword: password2
-            }
+              const bothPasswords = {
+                currentPassword: password1,
+                newPassword: password2
+              }
 
-            const data = encodeURIComponent(JSON.stringify(bothPasswords))
-            console.log('Sending current and new passwords:', data)
-            f('POST', '/api/' + loggedUser + '/update-password/' + data, response => {
-              console.log(response)
-            })
-          }}
-        />
-      </form>
-      <br />
-    </div>
+              const data = encodeURIComponent(JSON.stringify(bothPasswords))
+              console.log('Sending current and new passwords:', data)
+              f('POST', '/api/' + loggedUser + '/update-password/' + data, response => {
+                console.log(response)
+              })
+            }}
+          />
+        </form>
+      </div>
+    </span>
   )
 }
 export default Profile
