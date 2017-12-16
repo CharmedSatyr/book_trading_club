6fb9888a4b283cc987b37b5ab6bf029f20664384
CharmedSatyr <z@charmed.tech>
2017-12-17 00:38:40 +0900

    converted to single badge without icon that takes 3 color values

--- client/components/RequestsBadge.jsx
+++ client/components/RequestsBadge.jsx
@@ -9,22 +9,21 @@ import Badge from 'material-ui/Badge'
 import IconButton from 'material-ui/IconButton'
 import NotificationsIcon from 'material-ui/svg-icons/social/notifications'
 
+//Styles
+import { amber600, greenA700 } from 'material-ui/styles/colors'
+
 /*** MAIN ***/
-const RequestsBadge = ({ myRequests, requestsForMe }) => {
-  return (
-    <div>
-      <Badge badgeContent={myRequests} primary={true} className="badgeStyle">
-        <IconButton className="iconStyle" tooltip="Requests You've Made">
-          <NotificationsIcon />
-        </IconButton>
-      </Badge>
-      <Badge badgeContent={requestsForMe} secondary={true} className="badgeStyle">
-        <IconButton className="iconStyle" tooltip="Requests for You">
-          <NotificationsIcon />
-        </IconButton>
-      </Badge>
-    </div>
-  )
+const RequestsBadge = ({ badgeContent, color, tooltip }) => {
+  let badgeStyle = { right: 8, top: 19 }
+  let secondary = false
+  if (color === 'green') {
+    badgeStyle.backgroundColor = greenA700
+  } else if (color === 'yellow') {
+    badgeStyle.backgroundColor = amber600
+  } else {
+    secondary = true
+  }
+  return <Badge badgeContent={badgeContent} badgeStyle={badgeStyle} secondary={secondary} />
 }
 
 export default RequestsBadge
