603cced3dd1a7fac612602558483ddf8d115ac43
CharmedSatyr <z@charmed.tech>
2017-12-18 08:35:37 +0900

    +React Image package, implemented MUI RefreshIndicator (image loader)

--- client/components/Book.jsx
+++ client/components/Book.jsx
@@ -5,7 +5,14 @@
 import React from 'react'
 
 //Material UI
+import { amber600 } from 'material-ui/styles/colors'
+//Paper is only here because I started with it, and the WhichButton position
+//is currently built around it being here.
 import Paper from 'material-ui/Paper'
+import RefreshIndicator from 'material-ui/RefreshIndicator'
+
+//React Image
+import Img from 'react-image'
 
 //App
 import WhichButton from './WhichButton.jsx'
@@ -28,6 +35,21 @@ const Book = ({
   snackSwap,
   whichButton
 }) => {
+  const loader = (
+    <RefreshIndicator
+      size={50}
+      left={0}
+      top={0}
+      loadingColor={amber600}
+      status="loading"
+      style={{
+        display: 'inline-block',
+        position: 'relative',
+        zIndex: 0
+      }}
+    />
+  )
+
   return (
     <div className="book">
       <Paper
@@ -55,9 +77,10 @@ const Book = ({
           whichButton={whichButton}
         />
         <div className="img__wrap">
-          <img
+          <Img
             alt={title + ' (' + publication + ') by ' + author}
             className="img__img"
+            loader={loader}
             src={'https://covers.openlibrary.org/b/OLID/' + cover + '-M.jpg'}
             title={title + ' (' + publication + ') by ' + author}
           />
