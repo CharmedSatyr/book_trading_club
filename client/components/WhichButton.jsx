4003f92b2e973a8ff680a00234d776dc42aedd32
CharmedSatyr <z@charmed.tech>
2017-12-25 16:26:33 +0900

    WhichButton is now stateful & displays dialogs on approve/deny swaps

--- client/components/WhichButton.jsx
+++ client/components/WhichButton.jsx
@@ -1,25 +1,25 @@
 'use strict'
 
 /*** ENVIRONMENT ***/
-const path = process.cwd()
 import dotenv from 'dotenv'
 dotenv.load()
 
 /*** DEVELOPMENT TOOLS ***/
 const DEV = process.env.NODE_ENV === 'development'
-const PROD = process.env.NODE_ENV === 'production'
 
 /*** COMPONENTS ***/
 //React
-import React from 'react'
+import React, { Component } from 'react'
 
 //Material UI
 import { pinkA200, amber600, greenA700 } from 'material-ui/styles/colors'
+import ActionCheckCircle from 'material-ui/svg-icons/action/check-circle'
 import ActionSwapVerticalCircle from 'material-ui/svg-icons/action/swap-vertical-circle'
 import ContentAddCircle from 'material-ui/svg-icons/content/add-circle'
+import Dialog from 'material-ui/Dialog'
 import IconButton from 'material-ui/IconButton'
-import ActionCheckCircle from 'material-ui/svg-icons/action/check-circle'
 import NavigationCancel from 'material-ui/svg-icons/navigation/cancel'
+import RaisedButton from 'material-ui/RaisedButton'
 
 //Styles
 const iconStyle = {
@@ -34,203 +34,270 @@ const iconStyle = {
 import { f } from '../../common/common.functions.js'
 
 /*** MAIN ***/
-const WhichButton = ({
-  author,
-  cover,
-  loggedUser,
-  olkey,
-  owner,
-  publication,
-  requestor,
-  snackBar,
-  title,
-  whichButton
-}) => {
-  //This is basic inforation about the book that is appears in Icon Button tooltips on hover.
-  const reference = title + ' (' + publication + ') by ' + author
+export default class WhichButton extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { approveOpen: false, denyOpen: false }
+  }
 
-  //add button
-  const add = (
-    <span className="whichButton">
-      <span className="iconBox">
-        <IconButton
-          iconStyle={iconStyle}
-          onClick={() => {
-            const obj = {
-              author: author,
-              cover: cover,
-              olkey: olkey,
-              owner: loggedUser,
-              publication: publication,
-              title: title
-            }
-            const data = encodeURIComponent(JSON.stringify(obj))
-            console.log('Saving ' + title + ' by ' + author)
-            f('POST', '/api/' + loggedUser + '/save/' + data, response => console.log(response))
-            snackBar('add')
-          }}
-          tooltip={'Add ' + reference + ' to Your Books.'}
-        >
-          <ContentAddCircle color={greenA700} />
-        </IconButton>
-      </span>
-    </span>
-  )
+  render() {
+    //constants
+    const { approveOpen, denyOpen } = this.state
+    const {
+      author,
+      cover,
+      loggedUser,
+      olkey,
+      owner,
+      publication,
+      requestor,
+      snackBar,
+      title,
+      whichButton
+    } = this.props
 
-  //cancel button
-  const cancel = (
-    <span className="whichButton">
-      <span className="iconBox">
-        <IconButton
-          iconStyle={iconStyle}
-          onClick={() => {
-            const bookInfo = {
-              olkey: olkey,
-              owner: owner
-            }
-            const data = encodeURIComponent(JSON.stringify(bookInfo))
-            f('POST', '/api/' + loggedUser + '/cancelRequest/' + data, response => {
-              if (DEV) {
-                console.log(response)
-              }
-            })
-            snackBar('cancel')
-          }}
-          tooltip={'Cancel your request for ' + reference + ' from ' + owner}
-        >
-          <NavigationCancel color={pinkA200} />
-        </IconButton>
-      </span>
-    </span>
-  )
-  //delete button
-  const del = (
-    <span className="whichButton">
-      <span className="iconBox">
-        <IconButton
-          className="icon"
-          iconStyle={iconStyle}
-          onClick={() => {
-            const bookInfo = {
-              olkey: olkey,
-              owner: owner
-            }
-            const data = encodeURIComponent(JSON.stringify(bookInfo))
-            f('DELETE', '/api/' + owner + '/save/' + data, response => {
-              if (DEV) {
-                console.log(response)
-              }
-            })
-            snackBar('delete')
-          }}
-          tooltip={'Remove ' + reference + ' from Your Books.'}
-        >
-          <NavigationCancel color={pinkA200} />
-        </IconButton>
-      </span>
-    </span>
-  )
+    //This is basic inforation about the book that is appears in Icon Button tooltips on hover.
+    const reference = title + ' (' + publication + ') by ' + author
 
-  //swap button
-  const swap = (
-    <span className="whichButton">
-      <span className="iconBox">
-        <IconButton
-          iconStyle={iconStyle}
-          onClick={() => {
-            const bookInfo = {
-              olkey: olkey,
-              owner: owner
-            }
-            const data = encodeURIComponent(JSON.stringify(bookInfo))
-            f('POST', '/api/' + requestor + '/request/' + data, response => {
-              if (DEV) {
-                console.log(response)
+    //add button
+    const add = (
+      <span className="whichButton">
+        <span className="iconBox">
+          <IconButton
+            iconStyle={iconStyle}
+            onClick={() => {
+              const obj = {
+                author: author,
+                cover: cover,
+                olkey: olkey,
+                owner: loggedUser,
+                publication: publication,
+                title: title
               }
-            })
-            snackBar('swap')
-          }}
-          tooltip={'Request ' + reference + ' from ' + owner}
-        >
-          <ActionSwapVerticalCircle color={amber600} />
-        </IconButton>
+              const data = encodeURIComponent(JSON.stringify(obj))
+              console.log('Saving ' + title + ' by ' + author)
+              f('POST', '/api/' + loggedUser + '/save/' + data, response => console.log(response))
+              snackBar('add')
+            }}
+            tooltip={'Add ' + reference + ' to Your Books.'}
+          >
+            <ContentAddCircle color={greenA700} />
+          </IconButton>
+        </span>
       </span>
-    </span>
-  )
+    )
 
-  //approveDeny button
-  const approveDeny = (
-    <span className="whichButton">
-      <span className="iconBox">
-        {/* APPROVE REQUEST */}
-        <IconButton
-          iconStyle={iconStyle}
-          style={{ marginRight: -15 }}
-          onClick={() => {
-            const bookInfo = {
-              olkey: olkey,
-              owner: owner
-            }
-            const data = encodeURIComponent(JSON.stringify(bookInfo))
-            f('POST', '/api/' + loggedUser + '/approveRequest/' + data, response => {
-              if (DEV) {
-                console.log(response)
+    //cancel button
+    const cancel = (
+      <span className="whichButton">
+        <span className="iconBox">
+          <IconButton
+            iconStyle={iconStyle}
+            onClick={() => {
+              const bookInfo = {
+                olkey: olkey,
+                owner: owner
               }
-              alert(
-                'You have approved a request! See you at Beached Bar this Tuesday night at 8:30pm.'
-              )
-            })
-            snackBar('approve')
-          }}
-          tooltip={'Approve swap of ' + reference + ' with ' + requestor}
-        >
-          <ActionCheckCircle color={greenA700} />
-        </IconButton>
-        {/* DENY REQUEST */}
-        <IconButton
-          iconStyle={iconStyle}
-          onClick={() => {
-            const bookInfo = {
-              olkey: olkey,
-              owner: owner
-            }
-            const data = encodeURIComponent(JSON.stringify(bookInfo))
-            f('POST', '/api/' + loggedUser + '/denyRequest/' + data, response => {
-              if (DEV) {
-                console.log(response)
-              }
-            })
-            snackBar('deny')
-          }}
-          className="approveDenyStyle"
-          tooltip={'Deny swap of ' + reference + ' with ' + requestor}
-        >
-          <NavigationCancel color={pinkA200} />
-        </IconButton>
+              const data = encodeURIComponent(JSON.stringify(bookInfo))
+              f('POST', '/api/' + loggedUser + '/cancelRequest/' + data, response => {
+                if (DEV) {
+                  console.log(response)
+                }
+              })
+              snackBar('cancel')
+            }}
+            tooltip={'Cancel your request for ' + reference + ' from ' + owner}
+          >
+            <NavigationCancel color={pinkA200} />
+          </IconButton>
+        </span>
       </span>
-    </span>
-  )
+    )
+    //delete button
+    const del = (
+      <span className="whichButton">
+        <span className="iconBox">
+          <IconButton
+            className="icon"
+            iconStyle={iconStyle}
+            onClick={() => {
+              const bookInfo = {
+                olkey: olkey,
+                owner: owner
+              }
+              const data = encodeURIComponent(JSON.stringify(bookInfo))
+              f('DELETE', '/api/' + owner + '/save/' + data, response => {
+                if (DEV) {
+                  console.log(response)
+                }
+              })
+              snackBar('delete')
+            }}
+            tooltip={'Remove ' + reference + ' from Your Books.'}
+          >
+            <NavigationCancel color={pinkA200} />
+          </IconButton>
+        </span>
+      </span>
+    )
 
-  //return button based on whichButton prop
-  switch (whichButton) {
-    case 'add':
-      return add
-      break
-    case 'delete':
-      return del
-      break
-    case 'swap':
-      return swap
-      break
-    case 'approveDeny':
-      return approveDeny
-      break
-    case 'cancel':
-      return cancel
-      break
-    default:
-      return null
+    //swap button
+    const swap = (
+      <span className="whichButton">
+        <span className="iconBox">
+          <IconButton
+            iconStyle={iconStyle}
+            onClick={() => {
+              const bookInfo = {
+                olkey: olkey,
+                owner: owner
+              }
+              const data = encodeURIComponent(JSON.stringify(bookInfo))
+              f('POST', '/api/' + requestor + '/request/' + data, response => {
+                if (DEV) {
+                  console.log(response)
+                }
+              })
+              snackBar('swap')
+            }}
+            tooltip={'Request ' + reference + ' from ' + owner}
+          >
+            <ActionSwapVerticalCircle color={amber600} />
+          </IconButton>
+        </span>
+      </span>
+    )
+
+    //approveDeny button and confirmation dialogs
+    //Approve dialog
+    const confirmBtn = (
+      <RaisedButton
+        label="Confirm"
+        onClick={() => {
+          const bookInfo = {
+            olkey: olkey,
+            owner: owner
+          }
+          const data = encodeURIComponent(JSON.stringify(bookInfo))
+          f('POST', '/api/' + loggedUser + '/approveRequest/' + data, response => {
+            if (DEV) {
+              console.log(response)
+            }
+          })
+          snackBar('approve')
+        }}
+        primary={true}
+      />
+    )
+    const cancelBtn = (
+      <RaisedButton
+        className="rightBtn"
+        label="Cancel"
+        onClick={() => {
+          this.setState({ approveOpen: false })
+        }}
+        secondary={true}
+      />
+    )
+
+    const confirmDialog = (
+      <Dialog
+        actions={[confirmBtn, cancelBtn]}
+        modal={true}
+        open={approveOpen}
+        title="Swap complete?"
+      >
+        Have you already swapped this book with its requestor? Once you click Confirm, your book
+        will move to their collection. Hit Cancel if you haven't swapped yet.
+      </Dialog>
+    )
+
+    //Deny dialog
+    const denyBtn = (
+      <RaisedButton
+        label="Confirm"
+        onClick={() => {
+          const bookInfo = {
+            olkey: olkey,
+            owner: owner
+          }
+          const data = encodeURIComponent(JSON.stringify(bookInfo))
+          f('POST', '/api/' + loggedUser + '/denyRequest/' + data, response => {
+            if (DEV) {
+              console.log(response)
+            }
+          })
+          snackBar('deny')
+        }}
+        primary={true}
+      />
+    )
+    const cancelDenyBtn = (
+      <RaisedButton
+        className="rightBtn"
+        label="Cancel"
+        onClick={() => {
+          this.setState({ denyOpen: false })
+        }}
+        secondary={true}
+      />
+    )
+
+    const denyDialog = (
+      <Dialog actions={[denyBtn, cancelDenyBtn]} modal={true} open={denyOpen} title="Deny request?">
+        Hit Confirm if you're not able to swap this book with its requestor. Note that you can
+        remove this book from your collection in your Dashboard. Hit Cancel to take no action.
+      </Dialog>
+    )
+
+    const approveDeny = (
+      <span className="whichButton">
+        {confirmDialog}
+        {denyDialog}
+        <span className="iconBox">
+          {/* APPROVE REQUEST */}
+          <IconButton
+            iconStyle={iconStyle}
+            style={{ marginRight: -15 }}
+            onClick={() => this.setState({ approveOpen: true })}
+            tooltip={'Confirm swap of ' + reference + ' with ' + requestor}
+          >
+            <ActionCheckCircle color={greenA700} />
+          </IconButton>
+          {/* DENY REQUEST */}
+          <IconButton
+            iconStyle={iconStyle}
+            onClick={() => {
+              this.setState({ denyOpen: true })
+            }}
+            className="approveDenyStyle"
+            tooltip={'Deny request from ' + requestor + ' for ' + reference}
+          >
+            <NavigationCancel color={pinkA200} />
+          </IconButton>
+        </span>
+      </span>
+    )
+
+    //return button based on whichButton prop
+    switch (whichButton) {
+      case 'add':
+        return add
+        break
+      case 'delete':
+        return del
+        break
+      case 'swap':
+        return swap
+        break
+      case 'approveDeny':
+        return approveDeny
+        break
+      case 'cancel':
+        return cancel
+        break
+      default:
+        return null
+    }
   }
 }
-
-export default WhichButton
